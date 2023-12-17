ca27ddbc3192ca83751c9a3805d42ce108d31f90
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-12-17 08:16:54 +0500

    Changes

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ async function newsApi() {
       "https://newsapi.org/v2/everything?q=bitcoin&apiKey=3a02b8c644bd4df29d5e607607d4105a"
     );
     const data = await response.json();
-    console.log(data);
-    data.articles.map((news) => {
+    const newsData = data.articles;
+    newsData.map((news) => {
       newsBlock.innerHTML += `
-      
+      <div class="card" style="width: 19rem;">
+  <img class="card-img-top" src="${news.urlToImage}" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">${news.title}</h5>
+    <p class="card-text">${news.description}</p>
+    <a href="${news.url}" target="_blank" class="btn btn-dark">Read More</a>
+  </div>
+</div>
             `;
     });
   } catch (error) {
